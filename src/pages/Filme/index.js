dc02c86b7bca5b5cf8009611030741434a8d33a8
mamadusama <seu_email@example.commamadusamadev@gmail.com>
2024-08-01 07:54:25 +0100

    feat: add Film and Home components , Routs setup

---
+++ src/pages/Filme/index.js
@@ -0,0 +1,9 @@
+const Filme = () => {
+    return ( 
+        <div>
+        <h1>Bem vido a pagina de detalhes de Filme</h1>
+        </div>
+     );
+}
+ 
+export default Filme;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return ( 
+        <div>
+        <h>Bem vido a home</h>
+        </div>
+     );
+}
+ 
+export default Home;
